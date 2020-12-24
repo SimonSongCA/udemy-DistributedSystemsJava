02681e95fa1ae4f0a7b9a87769b4cb93bedd676e
SimonSongCA <SimonSongCA@gmail.com>
2020-12-24 13:52:08 -0800

    implemented the Leader Election in a JAR file; the JAR file is generated using 'java -jar leader.election-1.0-SNAPSHOT-jar-with-dependencies.jar' under the 'target' directory.

--- project/src/main/java/LeaderElection.java
+++ project/src/main/java/LeaderElection.java
@@ -14,14 +14,49 @@ public class LeaderElection implements Watcher {
     private static final int SESSION_TIMEOUT = 3000;
     // a one-stop shop for all the communication of our zookeeper server
     private ZooKeeper zooKeeper;
+    // define the z-node prefix
+    private static final String ELECTION_NAMESPACE = "/election";
+    // the currentZNodeName variable used to store the current name of the z-node
+    private String currentZnodeName;
 
     public static void main(String[] arg) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
-
         leaderElection.connectToZookeeper();
+
+        // volunteer as a leader and elect a leader.
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
+
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application");
+    }
+
+    public void volunteerForLeadership() throws KeeperException, InterruptedException {
+        // znode prefix. 'c' stands for 'candidate'
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        // call the znode create() method to create a znodeFullPath using znodePrefix
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        // print out the znode full path
+        System.out.println("znode name " + znodeFullPath);
+        // the shortened version of the path is used to assign the variable 'currentZnodeName'
+        this.currentZnodeName = znodeFullPath.replace("/election/", "");
+    }
+
+    public void electLeader() throws KeeperException, InterruptedException {
+        // call getChildren() to get a list of the children z-node of the election z-node
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+        // sort the list
+        Collections.sort(children);
+        // get the child with the smallest index. This will be the leader among all the other z-nodes
+        String smallestChild = children.get(0);
+        // determine whether the currentZnodeName is the smallest one.
+        if (smallestChild.equals(currentZnodeName)) {
+            System.out.println("I am the leader");
+            return;
+        }
+
+        System.out.println("I am not the leader, " + smallestChild + " is the leader");
     }
 
     public void connectToZookeeper() throws IOException {
